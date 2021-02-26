ee52c69e4ff72df33f4f0621954870a80f4fcdc3
Vikrant Kataria <vik7am@gmail.com>
2021-02-26 20:55:56 +0530

    Index pages static files added

--- rjp/myapp/urls.py
+++ rjp/myapp/urls.py
@@ -5,6 +5,9 @@ from .views import *
 app_name = 'jp1'
 urlpatterns = [
     path("index-2/", index_view_2),
+    path("index2/", index_2_view),
+    path("index3/", index_3_view),
+    path("index4/", index_4_view),
     path("about-us/", about_view),
     path("", index_view),
 ]
--- rjp/myapp/views.py
+++ rjp/myapp/views.py
@@ -11,6 +11,21 @@ def index_view_2(request):
         return render(request, "index.html", {})
 
 
+def index_2_view(request):
+    if request.method == "GET":
+        return render(request, "index2.html", {})
+
+
+def index_3_view(request):
+    if request.method == "GET":
+        return render(request, "index3.html", {})
+
+
+def index_4_view(request):
+    if request.method == "GET":
+        return render(request, "index4.html", {})
+
+
 def about_view(request):
     if request.method == "GET":
         return render(request, "about-us.html", {})
